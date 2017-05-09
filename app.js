a5b904975c3d50488c01ebc328727216280f4d97
MrCooper42 <mr.cooper42co@gmail.com>
2017-05-09 01:53:10 -0600

    cors test

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
 
     next();
 }
